a95561bf39be54a8d15c8d67ef9e86e79bcc1e1a
ashish <vermanashish@gmail.com>
2017-10-08 19:34:39 +0530

    validation

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-(function)({
-  'use strict';
+(function () {
+'use strict';
 
-  app.module('MyApp', [])
-  .controller('myController', ListController);
-  function ListController();
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+ 
+  $scope.splitString = function(name) {
+  	if ($scope.userForm.$valid) {
+  var arrayOfStrings = name.split(",");
+  	console.log(arrayOfStrings);
+  	if (arrayOfStrings.length <= 3 && arrayOfStrings.length > 0) {
+		console.log("Happy");
+		$scope.msg = "Enjoy!";
+	}
+		else{
+		$scope.msg = "Too much!";
+	}
+}
+else{
+	$scope.msg = "string is empty";
+}
+}
+}
+
 })();
